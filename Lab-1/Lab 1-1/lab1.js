56c73bfc1308de9c4550d64b8a4d3f79d11749c0
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-01-21 11:46:52 -0500

    added Lab-1

---
+++ Lab-1/Lab 1-1/lab1.js
@@ -0,0 +1,9 @@
+// alert("Hello World!")
+var name = "Mayuresh";
+var age = 25;
+var daysInYear = 365;
+var daysOld = age * daysInYear;
+var daysOldText = " days old.. more or less.";
+
+alert(daysOld);
+alert(name + " is " + daysOld + daysOldText);
