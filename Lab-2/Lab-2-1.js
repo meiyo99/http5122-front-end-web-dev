055f4ede7e0888619e9e4784ce267fb74dc4a017
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-01-21 13:05:45 -0500

    Lab2-1 completed

--- Lab-2/Lab-2-1.js
+++ Lab-2/Lab-2-1.js
@@ -1,5 +1,5 @@
 //######## LAB 2-1 FILE DELETE CONFIRMATION ########
-alert("hey 2.1");//COMMENT OUT ONCE CONNECTED TO YOUR HTML PAGE
+//alert("hey 2.1");//COMMENT OUT ONCE CONNECTED TO YOUR HTML PAGE
 //==== VARIABLES ========
 var userChoice;
 var messageOut = "Thank you, your file has ";
@@ -9,12 +9,16 @@ var noMessage = "not been altered.";
 //==== LOGIC ========
 
 //1. CREATE POPUP TO ASK FOR PERMISSION TO PROCEED WITH DELETION OF FILE.
-
-
+var userInput = confirm("Would you like to delete the file?");
+console.log(userInput);
 //2. IF USER CLICKS OKAY, POPUP messageOut WITH yesMessage.
-
+if(userInput === true) {
+    alert(messageOut + yesMessage);
+}
 
 //3. IF USER CLICKS ANYTHING BUT OKAY, POPUP messageOut WITH noMessage.
-
+else {
+    alert(messageOut + noMessage);
+}
 
 
