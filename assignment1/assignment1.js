5db405aecba557559e6d131e78c13484c6c653bb
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-01-22 18:28:40 -0500

    created and linked JS file

---
+++ assignment1/assignment1.js
@@ -0,0 +1,10 @@
+//######## ASSIGNMENT-1 GROUP LOGIN  ########
+
+//==== VARIABLES =========
+
+
+
+//==== LOGIC =============
+
+var userInput = prompt("Which team number do you belong to?");
+console.log(userInput);
