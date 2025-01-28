e558ef1083adde5be14175bc3cb59d97783e1692
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-01-28 13:03:34 -0500

    added week3

---
+++ week3/lab3/Lab-3-1.js
@@ -0,0 +1,8 @@
+//LAB 3 - ARRAYS & LOOPS - PART 1
+
+
+//ARRAY OF FRUITS
+var myFruits = ["Apple", "Mango", "Grapes", "Bananas", "Kiwi"];
+
+//OUTPUT ONE FRUIT FROM THE ARRAY IN POPUP BOX
+console.log(myFruits[2]);
