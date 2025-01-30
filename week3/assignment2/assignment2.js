17b26cb17ffade6ce96e70bbb9a8be00a423348e
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-01-29 19:50:06 -0500

    added variables

---
+++ week3/assignment2/assignment2.js
@@ -0,0 +1,8 @@
+var firstMovie = "Truman Show";
+var secondMovie = "Grinch";
+var thirdMovie = "Bruce Almighty";
+var fourthMovie = "Ace Ventura";
+var fifthMovie = "Liar Liar";
+var sixthMovie = "Eternal Sunshine of the Spotless Mind";
+var seventhMovie = "The Mask";
+var movieList = [firstMovie, secondMovie, thirdMovie, fourthMovie, fifthMovie, sixthMovie, seventhMovie];
