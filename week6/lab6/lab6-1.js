aefa4f0a1d5e9d9af81ef645f0164b4aa3653b95
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-02-20 22:50:06 -0500

    added lab6

--- week6/lab6/lab6-1.js
+++ week6/lab6/lab6-1.js
@@ -12,17 +12,17 @@ function pageReady(){
 	var image1 = document.getElementById("pic1");
 	var image2 = document.getElementById("pic2");
 	var image3 = document.getElementById("pic3");
-	//image4
-	//image5
-	//image6
+	var image4 = document.getElementById("pic4");
+	var image5 = document.getElementById("pic5");
+	var image6 = document.getElementById("pic6");
 
 	//SETUP LISTENERS FOR THE GALLERY IMAGES
 	image1.onmouseover = switchPic1;
 	image2.onmouseover = switchPic2;
 	image3.onmouseover = switchPic3;
-	//
-	//
-	//
+	image4.onmouseover = switchPic4;
+	image5.onmouseover = switchPic5;
+	image6.onmouseover = switchPic6;
 
 	//CREATE FUNCTIONS TO CHANGE PICTURES
 	function switchPic1(){
@@ -37,6 +37,18 @@ function pageReady(){
 		bigImage.src = image3.src;
 	}
 
+	function switchPic4(){
+		bigImage.src = image4.src;
+	}
+
+	function switchPic5(){
+		bigImage.src = image5.src;
+	}
+
+	function switchPic6(){
+		bigImage.src = image6.src;
+	}
+
 	//CREATE FUNCTION TO RESET IMAGE
 	function resetPic(){
 			bigImage.src = "images/title.png";
@@ -47,7 +59,7 @@ function pageReady(){
 	image1.onmouseout = resetPic;
 	image2.onmouseout = resetPic;
 	image3.onmouseout = resetPic;
-	//
-	//
-	//
+	image4.onmouseout = resetPic;
+	image5.onmouseout = resetPic;
+	image6.onmouseout = resetPic;
 }//END onload FUNCTION
--- week6/lab6/lab6-2.js
+++ week6/lab6/lab6-2.js
@@ -2,21 +2,35 @@
 //PART 2: MYSTERY BOX
 
 //LISTEN FOR load EVENT
-
+window.onload = pageReady;
 
 //'WRAPPER' FUNCTION FOR DOM LOGIC
-
+function pageReady(){
 	//GET DOM ELEMENTS WE'LL NEED
 
-
+	var mysteryBox = document.getElementById("mysteryBox");
+	var buttonBox = document.getElementById("buttonBox");
+	var head2 = document.getElementsByTagName("h2")[1];
 	//====CREATE THE FUNCTIONS WE'LL NEED====
+
 	//FUNCTION TO ASK USER
 
-
+	function popupAsk(){
+		var userInput = confirm("Do you want to see something?");
+		if(userInput === true) {
+			mysteryBox.style.display = "none";
+			buttonBox.style.display = "block";
+		}
+	}
 	//FUNCTION TO CHANGE buttonBox
-
+	function surprise(){
+		buttonBox.style.width = "600px";
+		buttonBox.style.backgroundColor = "orange";
+		head2.innerHTML = "SURPRISE!!";
+	}
 
 	//SETUP LISTENERS
+	mysteryBox.onmouseover = popupAsk;
+	buttonBox.onclick = surprise;
 
-
-//END onload FUNCTION
+}//END onload FUNCTION
