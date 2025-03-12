dd57844ade5df1e5ce88e2b5eea3c4ab50f8490c
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-03-11 21:57:02 -0400

    added lab7 files

---
+++ week7/JS-LAB-7-FormValidation25w/Lab-7-forms.js
@@ -0,0 +1,14 @@
+/* LAB 7 - SHIPPING FORM */
+//Order Shipping object (for extra EXTRA challenge, otherwise, ignore it)
+var shipInfo = {
+	client: "",
+	post: "",
+	speed: "",
+	cost: 0
+};
+
+//==== PLAN YOUR CODE WITH COMMENTS FIRST
+
+//LISTEN FOR WINDOW.ONLOAD EVENT
+
+
