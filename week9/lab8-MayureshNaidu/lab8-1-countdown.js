a4c0ff41f459c08a7148977e6a89853ab35860f2
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-03-18 14:56:51 -0400

    solbed lab8 and added submission files

---
+++ week9/lab8-MayureshNaidu/lab8-1-countdown.js
@@ -0,0 +1,30 @@
+﻿/* LAB 8-1 - FINAL COUNTDOWN!! */
+
+
+//create page load listener
+window.onload = projectDue;
+//create page load function
+function projectDue() {
+	//create variables for required HTML elements
+	
+	
+	//create variables for now date and due date
+	var nowDate = new Date();
+	var dueDate = new Date("April 22, 2024, 18:00:00");
+	//OUTPUT NOW DATE & DUE DATE TO HTML PAGE
+	todayData.innerHTML = nowDate.toDateString();
+	finalData.innerHTML = dueDate.toDateString();
+	//CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
+	var nowDateInt = nowDate.getTime()
+	var dueDateInt = dueDate.getTime()
+	var timeDiff = dueDateInt - nowDateInt;
+	//CONVERT TIME DIFFERENCE TO WHOLE NUMBER OF DAYS
+	var daysTillDue = timeDiff/86400000;
+	var FullDays = Math.floor(daysTillDue);
+	//LOGIC TO CHECK IF DUE DATE HAS PASSED, AND OUPUT APPROPRIATE MESSAGE TO HTML PAGE
+	if(FullDays <= 0) {
+		countMsg.innerHTML = "The deadline for the Pet Project has passed!";
+	} else {
+		dueData.innerHTML = FullDays;
+	}
+}
---
+++ week9/lab8-MayureshNaidu/lab8-2-clock.js
@@ -0,0 +1,41 @@
+﻿/* LAB 8.2 - STOP TIME */
+
+
+//create page load listener
+window.onload = Timer;
+//create page load function
+
+function Timer() {
+	//create variables for required HTML elements
+	var startBtn =  document.getElementById("btnStart");
+	var stopBtn =  document.getElementById("btnStop");
+	//create time variable so all functions have access to it
+	var hour;
+	var minute;
+	var second;
+	var interval = null;
+	//CREATE FUNCTION THAT DISPLAYS THE TIME
+	function displayTime() {
+		var todaysdate = new Date()
+		hour = String(todaysdate.getHours()).padStart(2, "0");
+		// console.log(hour)
+		minute = String(todaysdate.getMinutes()).padStart(2, "0");
+		// console.log(minute)
+		second = String(todaysdate.getSeconds()).padStart(2, "0");
+		// console.log(second)
+		hoursOut.innerHTML = hour;
+		minsOut.innerHTML = ":" + minute;
+		secsOut.innerHTML = ":" + second;
+		}
+	//CREATE FUNCTION TO START THE CLOCK.
+	function startClock() {
+		interval = setInterval(displayTime, 1000);
+	}
+	//CREATE FUNCTION TO STOP THE CLOCK
+	function stopClock() {
+		clearInterval(interval);
+	}
+	// SET EVENT LISTENERS
+	startBtn.onclick = startClock;
+	stopBtn.onclick = stopClock;
+}
