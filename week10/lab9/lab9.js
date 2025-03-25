10c4beb65716aae6259228d926ffdd99aa066d67
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-03-25 14:38:29 -0400

    added week10 and lab9

---
+++ week10/lab9/lab9.js
@@ -0,0 +1,55 @@
+﻿//LAB 9-DATA STORAGE: HOME PAGE
+window.onload = function() {
+
+    var formHandle = document.forms.infoForm;
+    formHandle.onsubmit = processForm;
+    var nameOut =  document.getElementById("newMsgBox");
+    var deleteBtn =  document.getElementById("btnDel");
+
+    //check for stored values
+        //retrieve stored values
+    var userOut = localStorage.getItem("inName");
+    var colourOut = localStorage.getItem("inColour");
+        //change welcome text to stored name
+    if (userOut !== null) {
+        nameOut.innerHTML = "Welcome, " + userOut + "!";
+    }
+    if (colourOut !== null) {
+        document.body.style.background = colourOut;
+    }
+        //change BG colour to stored colour
+
+        //  document.body.style.background = 
+
+
+		
+//#####============== DO THIS PART FIRST! ===============		
+    //get the form and set submit listener
+	
+	//onsubmit Function
+    function processForm() {
+
+        //get values from form
+		var userName = formHandle.f_name;
+		var userColour = formHandle.f_colour;
+
+		//console.log the form values
+        nameOut.innerHTML = "Welcome, " + userName.value + "!";
+        document.body.style.background = userColour.value;
+        console.log(userName.value);
+        console.log(userColour.value);
+
+        //store values
+        localStorage.setItem("inName", userName.value);
+        localStorage.setItem("inColour", userColour.value);
+    
+    }
+
+    function deleteData() {
+        localStorage.clear();
+        location.reload();
+    }
+    deleteBtn.onclick = deleteData;
+
+}
+    
---
+++ week10/lab9/product.js
@@ -0,0 +1,15 @@
+﻿window.onload = function() {
+
+    var nameOut =  document.getElementById("MesgBox");
+    //check for stored values
+        //retrieve stored values
+    var userOut = localStorage.getItem("inName");
+    var colourOut = localStorage.getItem("inColour");
+        //change welcome text to stored name
+    if (userOut !== null) {
+        nameOut.innerHTML = "Welcome, " + userOut + "!";
+    }
+    if (colourOut !== null) {
+        document.body.style.background = colourOut;
+    }
+}
